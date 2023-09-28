373a6e32d234d9313af931f113ad1d328afac597
Sujan Maji <dev.sujanmaji@gmail.com>
2023-09-28 19:21:33 +0530

    Modified Sagger to Production Mode

--- src/Services/Catalog.API/Program.cs
+++ src/Services/Catalog.API/Program.cs
@@ -17,11 +17,14 @@ builder.Services.AddScoped<IProductManager, ProductManager>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+/*if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+}*/
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
