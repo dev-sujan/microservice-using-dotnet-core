64a53e2d2d5f14bd7ee4065ff4fee6181f76622f
Sujan Maji <dev.sujanmaji@gmail.com>
2023-09-28 19:22:05 +0530

    Create Basket API

---
+++ src/Services/Busket.API/Models/ShoppingCart.cs
@@ -0,0 +1,29 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCart
+    {
+        public string Username { get; set; }
+
+        public ShoppingCart(string username)
+        {
+            Username = username;
+        }
+
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var item in Items)
+                {
+                    totalPrice += item.Price;
+                }
+                return totalPrice;
+            }
+        }
+
+
+    }
+}
