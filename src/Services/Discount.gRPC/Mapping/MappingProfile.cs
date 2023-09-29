2becb26e3ae2cdff7c7105813dbf2773d1618a30
Sujan Maji <dev.sujanmaji@gmail.com>
2023-09-29 22:49:55 +0530

    Implement Discount gRPC

---
+++ src/Services/Discount.gRPC/Mapping/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Discount.gRPC.Models;
+using Discount.gRPC.Protos;
+
+namespace Discount.gRPC.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Coupon, CouponRequest>().ReverseMap();
+        }
+    }
+}
---
+++ src/Services/Discount.gRPC/Repository/ICouponRepository.cs
@@ -0,0 +1,12 @@
+﻿using Discount.gRPC.Models;
+
+namespace Discount.gRPC.Repository
+{
+    public interface ICouponRepository
+    {
+        Task<Coupon> GetDiscount(string productId);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productId);
+    }
+}
